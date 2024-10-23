3ad0ebf31bde7ee6c7527faf0dadebbe86dddc1d
Ephrata <ephorata@gmail.com>
2024-10-23 11:43:26 +0300

    implement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,25 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName:'Ephrata', lastName:'Nebiyu'}
   return (
-   <main>
-       SIDEBAR
+   <main className="flex h-screen w-full font-inter">
+       <Sidebar user={loggedIn} />
+       <div className="flex size-full flex-col">
+         <div className="root-layout">
+           <Image src="icons/logo.svg" height={30} width={30} alt="menu icon" />
+           <div className="">
+             <MobileNav user={loggedIn} />
+           </div>
+         </div>
        {children}
+       </div>
    </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox"
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react"
 
 const Home = () => {
+  const loggedIn = { firstName:"Ephrata", lastName:'Nebiyu' ,email:"nebiyuephrata@gmail.com"};
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || "Guest"}
+          subtext ="Access and manage your account and transactions efficiently. " 
+          />
+
+          <TotalBalanceBox 
+           accounts={[]}
+           totalBanks={1}
+           totalCurrentBalance={1250.78}
+          />
+         </header>
+         {/* recent transactions */}
+      </div>
+      <RightSidebar user ={loggedIn} trasnactions={[]} banks={[{},{}]} />
+    </section>
   )
 }
 
